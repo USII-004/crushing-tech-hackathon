79e5c64e44053f7e623d7a453ab8d3c7cc91c2f6
usii-004 <usmansmooth004@gmail.com>
2023-11-23 23:18:12 +0100

    added toggle functionality to notification dropdown

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 /*DROPDOWN*/
 
+// User dropdown
+
 const userIcon = document.getElementById('user-info-container');
-const userDropdown = document.getElementById('user-dropdown')
+const userDropdown = document.getElementById('user-dropdown');
 
 let displayUserDropdown = false
 
@@ -10,8 +12,37 @@ function userDropdownDisplay() {
 }
 
 userIcon.addEventListener('click', () => {
-  displayUserDropdown = !displayUserDropdown;
-  userDropdownDisplay()
-  console.log(displayUserDropdown)
-  console.log(userDropdown.style.display)
+  if(displayNotificationDropdown) {
+    displayNotificationDropdown = !displayNotificationDropdown
+    notificationDropdownDisplay();
+    displayUserDropdown = !displayUserDropdown;
+    userDropdownDisplay();
+  }else {
+    displayUserDropdown = !displayUserDropdown;
+    userDropdownDisplay();
+  }
+})
+
+
+// Notification dropdown
+
+const notificationBell = document.getElementById('notification-bell-container');
+const notificationDropdown = document.getElementById('notification-dropdown');
+
+let displayNotificationDropdown = false;
+
+function notificationDropdownDisplay() {
+  notificationDropdown.style.display = displayNotificationDropdown ? 'block' : 'none';
+}
+
+notificationBell.addEventListener('click', () => {
+  if (displayUserDropdown) {
+    displayUserDropdown = !displayUserDropdown;
+    userDropdownDisplay()
+    displayNotificationDropdown = !displayNotificationDropdown;
+    notificationDropdownDisplay()
+  }else {
+    displayNotificationDropdown = !displayNotificationDropdown;
+    notificationDropdownDisplay()
+  }
 })
